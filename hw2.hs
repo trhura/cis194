9b9a89822c1436a784ff80275f09be40065e85ff
Thura Hlaing <trhura@gmail.com>
2018-08-20 22:30:57 +0630

    solved exercises for chapter 2

--- hw2.hs
+++ hw2.hs
@@ -1,15 +1,40 @@
 import Log
 
 parseWords :: [String] -> LogMessage
-parseWords [] = Unknown ""
-parseWords message@(x:y:z:xs) = case x of
-    "I" -> LogMessage Info (read y) (unwords $ z:xs)
-    "W" -> LogMessage Warning (read y) (unwords $ z:xs)
-    "E" -> LogMessage (Error $ read y) (read z) (unwords xs)
-    _ -> Unknown $ unwords message
+parseWords message@("I":ts:xs) = LogMessage Info (read ts) (unwords xs)
+parseWords message@("W":ts:xs) = LogMessage Warning (read ts) (unwords xs)
+parseWords message@("E":l:ts:xs)  = LogMessage (Error $ read l) (read ts) (unwords xs)
+parseWords others = Unknown $ unwords others
 
 parseMessage :: String -> LogMessage
 parseMessage = parseWords . words
 
 parse :: String -> [LogMessage]
 parse = (map parseMessage) . lines
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree = tree
+insert msg@(LogMessage _ _ _) (Leaf) = Node Leaf msg Leaf
+insert msg@(LogMessage _ ts _) (Node l nmsg@(LogMessage _ nts _) r)
+    | ts > nts = Node (insert msg l) nmsg r
+    | nts < ts = Node l nmsg (insert msg r)
+    | otherwise = Node l nmsg (insert msg r)
+
+build :: [LogMessage] -> MessageTree
+build [] = Leaf
+build (x:xs) = insert x $ build xs
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node l msg r) = (inOrder r) ++ [msg] ++ (inOrder l)
+
+isSevere :: LogMessage -> Bool
+isSevere (Unknown _) = False
+isSevere (LogMessage (Error serverity) _ _) = serverity >= 50
+isSevere (LogMessage _ _ _) = False
+
+justMessage :: LogMessage -> String
+justMessage (LogMessage _ _ msg) = msg
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong xs = map justMessage $ filter isSevere $ inOrder $ build xs
