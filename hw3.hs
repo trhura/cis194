1c71992abe8a4bd07ff2bf507bc5313328957999
Thura Hlaing <trhura@gmail.com>
2018-08-21 11:20:14 +0630

    sovled ex3 for ch3

--- hw3.hs
+++ hw3.hs
@@ -1,5 +1,7 @@
 module Golf where
 
+import Data.List
+
 getnth :: Int -> [a]  -> [a]
 getnth nth lst = [elm | (idx, elm) <- zip [1..] lst, idx `mod` nth == 0]
 
@@ -11,5 +13,25 @@ chunks n lst
     | length lst < n = []
     | otherwise = [take n lst] ++ chunks n (drop 1 lst)
 
-localMaxima :: (Ord a) => [a] -> [a]
+localMaxima :: Ord a => [a] -> [a]
 localMaxima lst = [b | (a:b:c:[]) <- (chunks 3 lst), b > a && b > c]
+
+count :: Eq a => a -> [a] -> Int
+count elm lst = length $ filter (== elm) lst
+
+counts :: (Num a, Enum a, Eq a) => [a] -> [Int]
+counts lst = [count x lst | x <- [0..9]]
+
+histLine :: Ord a => [a] -> a -> [Char]
+histLine counts lineNum = map (\x -> if x >= lineNum then '*' else ' ') counts
+
+histLines :: (Num a, Enum a, Eq a) => [a] -> [[Char]]
+histLines lst = reverse $ map (histLine cnts) [1..(maximum cnts)]
+                    where cnts = counts lst
+
+histogram lst = intercalate "\n" $
+                    histLines lst ++ [take 10 (repeat '=')] ++ [['0'..'9']] ++ ["\n"]
+
+
+
+
