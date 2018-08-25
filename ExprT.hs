bd71b2655772942394b3fde04af2a2f55641be3c
Thura Hlaing <trhura@gmail.com>
2018-08-25 22:13:52 +0630

    added ex6 for hw5

--- ExprT.hs
+++ ExprT.hs
@@ -4,6 +4,8 @@ newtype MinMax = MinMax Integer deriving (Eq, Ord, Show)
 newtype Mod7 = Mod7 Integer deriving (Eq, Show)
 
 data ExprT = Lit Integer
+           | Var String
            | Add ExprT ExprT
            | Mul ExprT ExprT
   deriving (Show, Eq)
+
--- hw5.hs
+++ hw5.hs
@@ -1,28 +1,36 @@
-{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 import ExprT
 import Parser
+import Control.Applicative
+import qualified Data.Map as M
 
--- eval :: ExprT -> Integer
--- eval (Lit x) = x
--- eval (Add expr1 expr2) = eval expr1 + eval expr2
--- eval (Mul expr1 expr2) = eval expr1 * eval expr2
+eval :: ExprT -> Integer
+eval (Lit x) = x
+eval (Add expr1 expr2) = eval expr1 + eval expr2
+eval (Mul expr1 expr2) = eval expr1 * eval expr2
 
--- evalStr :: String -> Maybe Integer
--- evalStr s = case parsed of
---         Nothing -> Nothing
---         Just exp -> Just $ eval exp
---     where parsed = parseExp Lit Add Mul s
+evalStr :: String -> Maybe Integer
+evalStr s = case parsed of
+        Nothing -> Nothing
+        Just exp -> Just $ eval exp
+    where parsed = parseExp Lit Add Mul s
 
 class Expr a where
     lit :: Integer -> a
     mul :: a -> a ->  a
     add :: a -> a ->  a
 
+class HasVars a where
+    var :: String -> a
+
 instance Expr ExprT where
     lit a = Lit a
     mul a b = Mul a b
     add a b  = Add a b
+
+instance HasVars ExprT where
+    var a = Var a
 
 instance Expr Integer where
     lit =  id
@@ -44,7 +52,15 @@ instance Expr Mod7 where
     add (Mod7 a) (Mod7 b) = Mod7 ((a + b) `mod` 7)
     mul (Mod7 a) (Mod7 b) = Mod7 ((a * b) `mod` 7)
 
-testExp :: Expr a => Maybe a
-testExp = parseExp lit add mul "(3 * -4) + 5"
+instance HasVars (M.Map String Integer -> Maybe Integer) where
+    var s = M.lookup s
 
+instance Expr (M.Map String Integer -> Maybe Integer) where
+    lit i = (\m -> Just i)
+    mul a b = (\m -> (*) <$> (a m) <*> (b m))
+    add a b = (\m -> (+) <$> (a m) <*> (b m))
 
+withVars :: [(String, Integer)]
+            -> (M.Map String Integer -> Maybe Integer)
+            -> Maybe Integer
+withVars vs exp = exp $ M.fromList vs
